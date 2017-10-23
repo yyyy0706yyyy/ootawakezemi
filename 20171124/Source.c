9b334f6040e693be0bacdb4d225069ce1e4cb63d
yutakayamazaki <yamazaki@lss.titech.ac.jp>
2017-10-23 22:53:51 +0900

    Add IOdata

---
+++ 20171124/Source.c
@@ -0,0 +1,42 @@
+#define _CRT_SECURE_NO_WARNINGS
+
+#include <stdio.h>
+#include <stdlib.h>
+
+
+struct player {
+	char name[20];
+	int OBP;/*èoó€ó¶*/
+	int AVE;/*1ó€ë≈à»è„ó¶*/
+	int AVE2;/*2ó€ë≈à»è„ó¶*/
+	int AVE3;/*3ó€ë≈à»è„ó¶*/
+	int HOMERUN;/*ñ{ó€ë≈ó¶*/
+	int RISP;/*ìæì_åóë≈ó¶*/
+};
+
+int main()
+{
+	FILE *fp;
+	char *filename = "data.txt";
+	char *output = "output.txt";
+	int i, j;
+	double data[17];
+
+	struct player pro[9];
+
+	if ((fp = fopen(filename, "r")) == NULL) {
+		printf("file open error\n");
+		exit(EXIT_FAILURE);
+	}
+	for (j = 0; j < 9; j++) {
+		fscanf(fp, "%s", pro[j].name);
+		printf("%s ", pro[j].name);
+		for (i = 0; i < 17; i++) {
+			fscanf(fp, "%lf", &data[i]);
+			printf("%f ", data[i]);
+		}
+		printf("\n");
+	}
+	fclose(fp);
+	return 0;
+}
