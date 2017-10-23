2f55e9bec4dbb346ebb47f460016f0cfc919d65a
yutakayamazaki <yamazaki@lss.titech.ac.jp>
2017-10-23 22:07:09 +0900

    Add data processing

--- 20171124/Source.cpp
+++ 20171124/Source.cpp
@@ -13,9 +13,27 @@ struct player {
 
 int main()
 {
+	FILE *fp;
+	char *filename = "data.txt";
+	char *output = "output.txt";
 	int i, j, k;
+	double data[22];
 
-	struct player pro[9] = {
+	struct player pro[9];
 
-	};
+	if ((fp = fopen(filename, "r")) == NULL) {
+		printf("file open error\n");
+		exit(EXIT_FAILURE);
+	}
+	for (j = 0; j < 9; j++) {
+		fscanf(fp, "%s", pro[j].name);
+		printf("%s", pro[j].name);
+		for (i = 0; i < 22; i++) {
+			fscanf(fp, "%lf", &data[i]);
+			printf("%f", data[i]);
+		}
+		printf("\n");
+	}
+	fclose(fp);
+	return 0;
 }
