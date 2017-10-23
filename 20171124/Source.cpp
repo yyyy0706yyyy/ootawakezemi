74f4db2fa05a6b41861392c4951ce2584bb31468
yutakayamazaki <yamazaki@lss.titech.ac.jp>
2017-10-23 20:48:18 +0900

    first commit

---
+++ 20171124/Source.cpp
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct player {
+	char name[20];
+	int OBP;/*èoó€ó¶*/
+	int AVE;/*1ó€ë≈à»è„ó¶*/
+	int AVE2;/*2ó€ë≈à»è„ó¶*/
+	int AVE3;/*3ó€ë≈à»è„ó¶*/
+	int HOMERUN;/*ñ{ó€ë≈ó¶*/
+	int RISP;/*ìæì_åóë≈ó¶*/
+};
+
+int main()
+{
+	int i, j, k;
+
+	struct player pro[9] = {
+		{"YAMADA" , 425 , 304 , 114 , } ,
+		{} ,
+		{} ,
+		{} ,
+		{} ,
+		{} ,
+		{} ,
+		{} ,
+		{} 
+	};
+}
